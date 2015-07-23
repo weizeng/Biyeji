0d5259afb1f65c29b3248a53334f2bbe57add8af
weizeng <leo.wei.badger@gmail.com>
2015-07-23 19:20:36 +0800

    添加.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -168,32 +168,34 @@ angular.module('starter.controllers', ['ngCordova'])
             $scope.modal = modal;
         });
         $scope.openModal = function () {
-            $scope.modal.show();
-            // 检测类型，看是否加载更多内容
-            var style = $scope.item.get('style');
-            var detailId = $scope.item.get('detailId');
-            if (style == 1) {
-                //短信息，直接显示
-                $scope.article = $scope.item.get('title');
-            } else {
-                $ionicLoading.show({template: '加载中...'});
-                // 实例方法
-                $timeout(function () {
-                    var XyDetail = Bmob.Object.extend("Xy_Detail");
-                    var query = new Bmob.Query(XyDetail);
-                    query.equalTo("objectId", detailId.id);
-                    // 查询所有数据
-                    query.first({
-                        success: function (results) {
-                            $ionicLoading.hide();
-                            $scope.article = results.get('extends');
-                        },
-                        error: function (error) {
-                            alert("查询失败: " + error.code + " " + error.message);
-                        }
-                    });
-                }, 500);
-            }
+            $scope.modal.show().then(function (obj) {
+                // 检测类型，看是否加载更多内容
+                var style = $scope.item.get('style');
+                var detailId = $scope.item.get('detailId');
+                if (style == 1) {
+                    //短信息，直接显示
+                    $scope.article = $scope.item.get('title');
+                } else {
+                    $ionicLoading.show({template: '加载中...'});
+                    // 实例方法
+//                    $timeout(function () {
+                        var XyDetail = Bmob.Object.extend("Xy_Detail");
+                        var query = new Bmob.Query(XyDetail);
+                        query.equalTo("objectId", detailId.id);
+                        // 查询所有数据
+                        query.first({
+                            success: function (results) {
+                                $ionicLoading.hide();
+                                $scope.article = results.get('extends');
+                            },
+                            error: function (error) {
+                                alert("查询失败: " + error.code + " " + error.message);
+                            }
+                        });
+//                    }, 500);
+                }
+            });
+
         };
         $scope.closeModal = function () {
             $scope.modal.hide();
@@ -211,6 +213,7 @@ angular.module('starter.controllers', ['ngCordova'])
 
         //TODO 保存简单的评论
         $scope.saveForm = function (xy) {
+            $ionicLoading.show({template: '评论中...'});
             // 添加到赞列表
             var commentObject = Bmob.Object.extend("Comment");
             // 插入许愿列表
@@ -221,6 +224,7 @@ angular.module('starter.controllers', ['ngCordova'])
             comment.set("content", $scope.msg);
             comment.save(null, {
                 success: function (comment) {
+                    $ionicLoading.hide();
                     // 添加成功之后，将之前查询到的评论信息的relation字段重置。关联起来
                     var relation = xy.relation("commentId");
                     relation.add(comment);
@@ -272,10 +276,10 @@ angular.module('starter.controllers', ['ngCordova'])
                     if (results.length > 0) {
                         $scope.more = true;
 
-                        if(skip == 0) {
+                        if (skip == 0) {
                             $scope.results.length = 0;
-                            $scope.imagesCount.length=0;
-                            $scope.commentCount.length=0;
+                            $scope.imagesCount.length = 0;
+                            $scope.commentCount.length = 0;
                             $scope.$broadcast('scroll.refreshComplete');
                         }
                         angular.forEach(results, function (result) {
@@ -303,9 +307,9 @@ angular.module('starter.controllers', ['ngCordova'])
                             $scope.results.push(result);
                         });
                         skip += results.length;
-                        console.log("skip:"+skip);
+                        console.log("skip:" + skip);
                     } else {
-                        if(skip == 0) {
+                        if (skip == 0) {
 
                         }
                         $scope.more = false;
@@ -436,53 +440,50 @@ angular.module('starter.controllers', ['ngCordova'])
         $scope.xy = {content: null};
 
         //TODO 增加一个许愿
-        $scope.addComment = function () {
-            var query = new Bmob.Query("_User");
-            query.first({
-                //查询要保存的用户，这个对象应该要被序列话到本地
-                success: function (user) {
-                    // FIXME 这里需要做第三方登陆，之后把成功的用户信息保存到本地，目前这里只是查询数据中第一条数据，模拟
+        $scope.addXy = function () {
+//            var query = new Bmob.Query("_User");
+//            query.first({
+//                //查询要保存的用户，这个对象应该要被序列话到本地
+//                success: function (user) {
+            // FIXME 这里需要做第三方登陆，之后把成功的用户信息保存到本地，目前这里只是查询数据中第一条数据，模拟
+            $ionicLoading.show({template: '发表中...'});
+//                    $rootScope.user = user;
+            // FIXME 上传宣言墙的图片, 这里只测试本地的字符串保存到BMOB的file字段
+//            var fileUploadControl = $("#profilePhotoFileUpload")[0];
+//            if (fileUploadControl.files.length > 0) {
+//                var file = fileUploadControl.files[0];
+                var name = "logo2.png";
+                var file = new Bmob.File(name, $scope.cameraimage,"image/png");
+                file.save().then(function (obj) {
+                    alert("url:"+obj.url());
+                    console.log(obj.url());
+                    var Xy_List = Bmob.Object.extend("Xy_List");
+                    // 插入许愿列表
+                    var ddd = new Xy_List();
+                    ddd.set("title", $scope.xy.content);
+                    // Pointer指针
+                    ddd.set("userId", $scope.user);
+                    ddd.set("image", obj);
+                    ddd.set("style", 1);
+                    alert("2222:" + $scope.xy.content);
+                    ddd.save(null, {
+                        success: function (ddd) {
 
-                    $rootScope.user = user;
-                    // FIXME 上传宣言墙的图片, 这里只测试本地的字符串保存到BMOB的file字段
-                    var bytes = "Hello, World!";
-                    var file = new Bmob.File("hello.txt", bytes);
-                    file.save().then(function (obj) {
-                        localStorage.setItem('data', JSON.stringify(user));
-                        var aa = localStorage.getItem('data');
-                        var outP = eval('(' + aa + ')');
-                        console.log(outP);
-
-
-                        var Xy_List = Bmob.Object.extend("Xy_List");
-                        // 插入许愿列表
-                        //var aa = Bmob.Query(Xy_List);
-                        var ddd = new Xy_List();
-
-                        ddd.set("title", $scope.xy.content);
-                        // Pointer指针
-                        ddd.set("userId", user);
-                        ddd.set("image", obj);
-
-                        ddd.save(null, {
-                            success: function (ddd) {
-                                alert("你的毕业说已经到宣言墙啦");
-                                var relation = ddd.relation("commentId");
-                            },
-                            error: function (ddd, error) {
-                                alert("抱歉，学长，错了。。" + error.message);
-                            }
-                        });
-                    }, function (error) {
-                        // the save failed.
+                            $ionicLoading.hide();
+                            alert("你的毕业说已经到宣言墙啦");
+                            var relation = ddd.relation("commentId");
+                        },
+                        error: function (ddd, error) {
+                            alert("抱歉，学长，错了。。" + error.message);
+                        }
                     });
-
-
-                },
-                error: function (error) {
-                    alert("查询失败: " + error.code + " " + error.message);
-                }
-            });
+                }, function (error) {
+                    // the save failed.
+                    alert("抱歉，学长，错了2。。" + error.message);
+                })
+//            } else {
+//                $ionicLoading.hide();
+//            }
         }
         //选择拍照
         $scope.goCamera = function () {
@@ -495,6 +496,7 @@ angular.module('starter.controllers', ['ngCordova'])
             // udpate camera image directive
             $cordovaCamera.getPicture(options).then(function (imageData) {
                 $scope.cameraimage = "data:image/jpeg;base64," + imageData;
+
                 //TODO 保存图片接口
             }, function (err) {
                 console.log('Failed because: ');
