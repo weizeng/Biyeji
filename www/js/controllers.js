fc00d865fb93b51977365059cc73d73439d1d8ee
weizeng <leo.wei.badger@gmail.com>
2015-07-29 18:43:33 +0800

    修改bug

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -157,8 +157,6 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
                         }
                         angular.forEach(results, function (result) {
                             result.htmlStr = $sce.trustAsHtml(result.get('title'));
-                            // FIXME 考虑后台提交file的时候直接产生缩略图 http://wenda.bmob.cn/?/question/129 在云端掺入url可以让云端保存呢一份缩略图，将返回的url保存到特殊字段中
-                            // 怀疑第二次请求会取本地的图片直接返回
                             var zanRelation = result.relation("zan");
                             zanRelation.query().find({
                                 success: function (list) {
@@ -207,7 +205,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
         $ionicLoading.show({template: '加载中...'});
 
-        if( window.sessionStorage.getItem('doRefresh')){
+        if (window.sessionStorage.getItem('doRefresh')) {
             window.sessionStorage.removeItem('doRefresh');
             $scope.hardRefresh();
         }
@@ -250,7 +248,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
                 result = parseInt(dayC) + "天前";
             }
             else if (hourC >= 1) {
-                result = parseInt(hourC) + "个小时前";
+                result = parseInt(hourC) + "小时前";
             }
             else if (minC >= 1) {
                 result = parseInt(minC) + "分钟前";
@@ -264,7 +262,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     })
 
 // 增加我的毕业说
-    .controller('AddXyCtrl', function ($cordovaDialogs, $ionicPlatform, $rootScope, $scope, $ionicLoading, $cordovaCamera, $cordovaFile) {
+    .controller('AddXyCtrl', function ($timeout, $cordovaDialogs, $ionicPlatform, $rootScope, $scope, $ionicLoading, $cordovaCamera, $cordovaFile) {
 
         //返回
         $scope.back = function () {
@@ -275,7 +273,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
         $scope.xy = {content: null};
 
         //初始化加载先清空
-        function initData(){
+        function initData() {
             $scope.xy = {content: null};
             $scope.cameraimage = null;
         };
@@ -293,74 +291,88 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
                 return;
             }
+            if (imageLocalPath == null) {
+                $.fn.umshare.tip("请先选择一张照片");
+                return;
+            }
 
             var name = imageLocalPath.substr(imageLocalPath.lastIndexOf('/') + 1);
-            document.addEventListener("deviceready", function () {
-                var picDictionary;
-                if (ionic.Platform.isAndroid()) {
-                    // 必须要加前缀file:// 否则出现 ECODING_ERR的错误
-                    picDictionary = 'file://' + imageLocalPath.substr(0, imageLocalPath.lastIndexOf('/'));
-                } else {
-                    picDictionary = cordova.file.tempDirectory;
-                }
 
-                console.log("#增加许愿#图片:" + picDictionary);
-                $cordovaFile.readAsBinaryString(picDictionary, name).then(function (success) {
-                    // success
+            var picDictionary;
+            if (ionic.Platform.isAndroid()) {
+                // 必须要加前缀file:// 否则出现 ECODING_ERR的错误
+                picDictionary = 'file://' + imageLocalPath.substr(0, imageLocalPath.lastIndexOf('/'));
+            } else {
+                picDictionary = cordova.file.tempDirectory;
+            }
+
+            console.log("准备读取图片===>" + picDictionary + name);
+            $ionicLoading.show({
+                template: ('读取图片中... ' + "<i class = 'ion-close-round' ng-click='forceLoading()'></i>")
+            });
+            $cordovaFile.readAsBinaryString(picDictionary, name).then(function (success) {
+                // success
+                var file = new Bmob.File(name, success);
+                $ionicLoading.show({
+                    template: ('图片读取完毕,开始上传... ' + "<i class = 'ion-close-round' ng-click='forceLoading()'></i>")
+                });
+                console.log("图片读取完毕,开始上传===>" + picDictionary + name);
+                file.save().then(function (savedObject) {
+//                        alert('图片保存成功:' + savedObject.url());
+                    console.log('图片保存成功,准备创建图片缩略图===>' + savedObject.url());
                     $ionicLoading.show({
-                        template: ('发表中... ' + "<i class = 'ion-close-round' ng-click='forceLoading()'></i>")
+                        template: ('正在创建缩略图... ' + "<i class = 'ion-close-round' ng-click='forceLoading()'></i>")
                     });
+                    Bmob.Image.thumbnail({"image": savedObject.url(), "mode": 0, "quality": 100, "width": 480}
+                    ).then(function (obj) {
+                            $ionicLoading.show({
+                                template: ('正在发布到毕业墙... ' + "<i class = 'ion-close-round' ng-click='forceLoading()'></i>")
+                            });
+                            console.log("-------- filename:" + JSON.stringify(obj));
 
-                    //$ionicLoading.show({template: '发表中...'});
-                    var file = new Bmob.File(name, success, "image/png");
-                    console.log("#增加许愿#图片读取完毕:" + picDictionary);
-                    file.save().then(function (savedObject) {
-                        // 生成缩略图
-                        Bmob.Image.thumbnail({"image": savedObject.url(), "mode": 0, "quality": 100, "width": 480}
-                        ).then(function (thumbnail) {
-                                console.log("#增加许愿#缩略图图片上传完毕, 准备添加到URL:" + thumbnail.url());
-                                var Xy_List = Bmob.Object.extend("Xy_List");
-                                // 插入许愿列表
-                                var ddd = new Xy_List();
-                                ddd.set("title", $scope.xy.content);
-                                // Pointer指针
-                                ddd.set("userId", $rootScope.user);
-                                ddd.set("image", savedObject);
-                                ddd.set("image_small", thumbnail.url);
-                                ddd.set("style", 1);
-                                ddd.save(null, {
-                                    success: function (ddd) {
-                                        $ionicLoading.hide();
-                                        console.log("#增加许愿# 操作完毕");
-//                                alert("你的毕业说已经到宣言墙啦");
-                                        var relation = ddd.relation("commentId");
+                            var Xy_List = Bmob.Object.extend("Xy_List");
+//                                    // 插入许愿列表
+                            var ddd = new Xy_List();
+                            ddd.set("title", $scope.xy.content);
+                            // Pointer指针
+                            ddd.set("userId", $rootScope.user);
+                            ddd.set("image", savedObject);
+                            ddd.set("image_small", obj.error == null ? obj.url : savedObject.url());
+                            ddd.set("style", 1);
+                            ddd.save(null, {
+                                success: function (ddd) {
+                                    $ionicLoading.hide();
+                                    console.log("#增加许愿# 操作完毕");
+                                    var relation = ddd.relation("commentId");
                                         $cordovaDialogs.confirm('你的毕业说已经到宣言墙啦', '太好了', '确定')
                                             .then(function () {
                                                 // callback success
-                                                window.sessionStorage.setItem('doRefresh',true);
+                                                window.sessionStorage.setItem('doRefresh', true);
                                                 $scope.back();
                                             });
-                                    },
-                                    error: function (ddd, error) {
-                                        $ionicLoading.hide();
-                                        alert("抱歉，学长，错了。。" + error.message);
-                                    }
-                                });
-
+                                },
+                                error: function (ddd, error) {
+                                    $ionicLoading.hide();
+                                    alert("抱歉，学长，错了。。" + error.message);
+                                }
                             });
-
-
-                    }, function (error) {
-                        // the save failed.
-                        $ionicLoading.hide();
-                        $cordovaDialogs.alert('网络在开小差' + JSON.stringify(error), '糟糕啊', '确定');
-                    });
-//                    alert("right");
+                        }, function (error) {
+                            // the save failed.
+                            $ionicLoading.hide();
+                            console.log("创建缩略图错误" + JSON.stringify(error));
+                        });
                 }, function (error) {
-                    // error
-                    $cordovaDialogs.alert('网络在开小差2' + JSON.stringify(error), '糟糕啊', '确定');
+                    // the save failed.
+                    $ionicLoading.hide();
+                    console.log("保存图错误" + JSON.stringify(error));
+                    $cordovaDialogs.alert('网络在开小差2222' + JSON.stringify(error), '糟糕啊', '确定');
                 });
+            }, function (error) {
+                // error
+                console.log('文件读取错误' + JSON.stringify(error));
+                $cordovaDialogs.alert('文件读取错误' + JSON.stringify(error), '糟糕啊', '确定');
             });
+
         };
         //选择拍照
         $scope.goCamera = function () {
@@ -410,6 +422,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
                 if (ionic.Platform.isAndroid()) {
                     window.FilePath.resolveNativePath(imageURI, function (success) {
                         imageLocalPath = success;
+
                     }, function (error) {
                         alert(error);
                     });
@@ -420,14 +433,13 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
                 // 用于展示
                 $scope.cameraimage = imageURI;
             }, function (err) {
-                console.log('Failed because: ');
-                console.log(err);
+                console.log('Failed because: ' + err);
             });
         }
     })
 
     // 我的毕业脚印
-    .controller('MyBoardCtrl', function ($rootScope, $scope, $http, $ionicLoading) {
+    .controller('MyBoardCtrl', function ($cordovaDialogs,$rootScope, $scope, $http, $ionicLoading) {
         $scope.logout = function () {
             var result = $.fn.umshare.delToken("sina");
             localStorage.removeItem('user');
